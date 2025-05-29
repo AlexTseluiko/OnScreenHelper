c41df259bebbe7e1c9b750e7843c4a5a83500831
AlexTseluiko <alexandertceluiko@gmail.com>
2025-05-29 16:39:20 +0300

    Rebrand MedHelper to OnScreen + signing keys setup

--- dist/assets/web-BuE95ONe.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-BC1i8pwM.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ dist/assets/web-COeFyGFl.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-BQBbN0Oo.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
